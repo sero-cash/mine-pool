752b040c33c4dfcc5b666856840fb6ff0672842a
shordingercat <tangzhige@gmail.com>
2019-03-01 15:49:38 +0800

    fix a bug

--- ethash/src/libethash/stub.h
+++ ethash/src/libethash/stub.h
@@ -9,27 +9,27 @@
 #include "mmap.h"
 
 inline void* stub_calloc(size_t n, size_t size, const char* name) {
-    printf("STUB CALLOC: %s", name);
+    printf("STUB CALLOC: %s\n", name);
     return calloc(n,size);
 }
 
 inline void* stub_malloc(size_t size, const char* name) {
-    printf("STUB MALLOC: %s", name);
+    printf("STUB MALLOC: %s\n", name);
     return malloc(size,name);
 }
 
 inline void stub_free(void* ptr,const char* name) {
-    printf("STUB FREE: %s", name);
+    printf("STUB FREE: %s\n", name);
     return free(ptr);
 }
 
 inline void* stub_mmap(void* start, size_t length, int prot, int flags, int fd, off_t offset,const char* name) {
-    printf("STUB MMAP: %s", name);
+    printf("STUB MMAP: %s\n", name);
     return mmap(start,length,prot,flags,fd,offset);
 }
 
 inline void stub_munmap(void* addr, size_t length,const char* name) {
-    printf("STUB NMAP: %s", name);
+    printf("STUB NMAP: %s\n", name);
     munmap(addr,length);
 }
 
